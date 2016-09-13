06b12d072747ec16dec3cee6c02a760e0443a9d9
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-13 12:54:23 -0600

    Implement sum28 function

--- arrays.py
+++ arrays.py
@@ -1,6 +1,15 @@
 def haveThree(a):
     threes = 0
-    for(i = 0; i < len(a); i += 1):
-        if (a[i] == 3):
+    for num in a:
+        if num == 3:
             threes += 1
     return threes == 3
+
+def sum28(b):
+    sum = 0
+    for num in b
+        if num == 2:
+            sum += 1
+    return sum == 4
+
+# Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
