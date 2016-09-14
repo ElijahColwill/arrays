00bb42c5fef79e90a9d5b986660dd3ecc455c964
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-14 11:57:33 -0600

    Implement refrence code frm learn python the hard way

--- arrays.py
+++ arrays.py
@@ -1,22 +1,31 @@
-def haveThree(a):
-    threes = 0
-    for num in a:
-        if num == 3:
-            threes += 1
-    return threes == 3
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-print haveThree([3, 4, 3, 4, 3])
-print haveThree([3, 5, 3,])
+for number in the_count:
+    print "This is count %d" % number
 
-def sum28(b):
-    sum = 0
-    for num in b:
-        if num == 2:
-            sum += 1
-    return sum == 4
 
-print sum28([2, 2, 2, 2])
-print sum28([2, 2, 2])
-print sum28([2, 2])
-print sum28([4, 2, 3, 2, 2, 2, 1])
-print sum28([2, 2, 2, 2, 2])
+for fruit in fruits:
+    print "A fruit of type: %s" % fruit
+
+for i in change:
+    print "I got %r" % i
+
+elements = []
+
+for i in range(0, 6):
+    print "Adding %d to the list." % i
+    elements.append(i)
+
+for i in elements:
+    print "Element was: %d" % i
+
+pizza = []
+
+for i in range(0, 13):
+    print "Number of pizza slics: %d" % i
+    pizza.append(i)
+
+for i in pizza:
+    print "Slices eaten: %d" % i
